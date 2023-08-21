15b83c883f054c050a1d5bab6bb60556da18d7eb
sandtory <henning.sandtorv@hotmail.com>
2023-08-21 15:52:00 +0200

    added tracking from applehealthkit

--- App.tsx
+++ App.tsx
@@ -2,16 +2,91 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import RingProgress from "./src/components/RingProgress";
 import Value from "./src/components/value";
+import AppleHealthKit, {
+  HealthInputOptions,
+  HealthKitPermissions,
+  HealthUnit,
+} from "react-native-health";
+import { useEffect, useState } from "react";
 
+const permissions: HealthKitPermissions = {
+  permissions: {
+    read: [
+      AppleHealthKit.Constants.Permissions.Steps,
+      AppleHealthKit.Constants.Permissions.FlightsClimbed,
+      AppleHealthKit.Constants.Permissions.DistanceWalkingRunning,
+    ],
+    write: [],
+  },
+};
+const STEPS_GOAL = 10_000;
 export default function App() {
+  const [hasPermissions, setHasPermission] = useState(false);
+  const [steps, setSteps] = useState(0);
+  const [flights, setFlights] = useState(0);
+  const [distance, setDistance] = useState(0);
+
+  useEffect(() => {
+    AppleHealthKit.initHealthKit(permissions, (err) => {
+      if (err) {
+        console.log("error getting permissions");
+        return;
+      }
+      setHasPermission(true);
+      // you can request data
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!hasPermissions) {
+      return;
+    }
+
+    const options: HealthInputOptions = {
+      date: new Date(2023, 5, 18).toISOString(),
+      includeManuallyAdded: false,
+    };
+
+    AppleHealthKit.getStepCount(options, (err, results) => {
+      if (err) {
+        console.log("Error getting steps");
+        return;
+      }
+      console.log(results);
+      setSteps(results.value);
+    });
+
+    AppleHealthKit.getFlightsClimbed(options, (err, results) => {
+      if (err) {
+        console.log("Error getting flights");
+        return;
+      }
+      console.log(results);
+      setFlights(results.value);
+    });
+
+    AppleHealthKit.getDistanceWalkingRunning(options, (err, results) => {
+      if (err) {
+        console.log("Error getting distance");
+        return;
+      }
+      console.log(results);
+      setDistance(results.value);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <RingProgress radius={150} strokeWidth={50} progress={0.1} />
+      <RingProgress
+        radius={150}
+        strokeWidth={50}
+        progress={steps / STEPS_GOAL}
+      />
 
       <View style={styles.values}>
-        <Value label="Steps" value="1200" />
-        <Value label="Distance" value="0,75km" />
-        <Value label="Flights climbed" value="35" />
+        <Value label="Steps" value={steps.toString()} />
+        <Value label="Distance" value={`${(distance / 1000).toFixed(2)}km`} />
+        <Value label="Flights climbed" value={flights.toString()} />
       </View>
 
       <StatusBar style="auto" />
